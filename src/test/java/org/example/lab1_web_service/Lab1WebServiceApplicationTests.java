c9c8ce1e90c3f002835fd09b13bc9af8bde6bc85
Torsten Wihlborg <torstencaous12@gmail.com>
2026-05-12 00:59:05 +0200

    Återställer koden till den nya paketstrukturen

--- src/test/java/org/example/lab1_web_service/Lab1WebServiceApplicationTests.java
+++ src/test/java/org/example/lab1_web_service/Lab1WebServiceApplicationTests.java
@@ -1,13 +1,36 @@
 package org.example.lab1_web_service;
 
+import org.example.lab1_web_service.DTO.Message;
+import org.example.lab1_web_service.Service.ChatHistoryManager;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest
 class Lab1WebServiceApplicationTests {
 
+    @Autowired
+    private ChatHistoryManager historyManager;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void  testChatHistoryStorage() {
+        String sessionId = "Test-session";
+        Message message = new Message("user", "Hej", sessionId);
+
+        historyManager.addMessage(sessionId, message);
+        List<Message> history = historyManager.getHistory(sessionId);
+
+        assertFalse(history.isEmpty(), "Historiken är tom, den ska inte vara tom");
+        assertEquals("Hej", history.get(0).content());
     }
 
 }
